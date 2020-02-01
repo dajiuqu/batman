7a27b1117167af54ef009452e25399e2aae3bd5f
mikewang <1085262498@qq.com>
2020-02-01 14:01:03 +0800

    init

--- coreweb/src/main/java/com/bigdata/coreweb/AklungApplication.java
+++ coreweb/src/main/java/com/bigdata/coreweb/AklungApplication.java
@@ -1,8 +1,10 @@
 package com.bigdata.coreweb;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.bigdata.coreweb.mapper")
 @SpringBootApplication
 public class AklungApplication {
 
